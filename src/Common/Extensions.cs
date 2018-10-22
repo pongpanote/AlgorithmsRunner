f28dfab41a9ee2b55a575a96381f654dec2f811c
Pongpanote.Gongsook <Pongpanote.Gongsook@schange.com>
2018-10-22 15:03:44 +0200

    97%

--- src/Common/Extensions.cs
+++ src/Common/Extensions.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Reflection;
 
 namespace AlgorithmsRunner.Common
@@ -9,11 +8,6 @@ namespace AlgorithmsRunner.Common
         public static T GetAttributeOfType<T>(this MemberInfo type)
         {
             return type.GetCustomAttributes(typeof(T), true).Cast<T>().FirstOrDefault();
-        }
-
-        public static IEnumerable<T> GetAttributesOfType<T>(this MemberInfo type)
-        {
-            return type.GetCustomAttributes(typeof(T), true).Cast<T>();
         }
     }
 }
